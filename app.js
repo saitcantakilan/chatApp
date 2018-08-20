cb936544154f7e78d0364bd9b7397b393778c89f
Saitcan TAKILAN <sait@kodanka.com>
2018-08-20 23:04:46 +0300

    auth route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const auth = require('./routes/auth');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const passportGoogle = require('../auth/google');
+
+router.get('/google', passportGoogle.authenticate(
+    'google',
+    {
+        scope: ['profile', 'email']
+    }
+));
+
+router.get('google/callback', passportGoogle.authenticate(
+    'google',
+    {
+        failureRedirect: '/'
+    }),
+    (req,res) => {
+        res.redirect('chat');
+    });
+
+    module.exports = router;
